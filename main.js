c96dd8762c5ded65542caf05ce38bbffc917f108
Handi9322 <134509716+Handi9322@users.noreply.github.com>
2024-07-28 00:52:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+//toggle and responsive  navigasi
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navList = document.querySelector("nav")
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+}
+
+navSlide();
+
+// hapus form setelah mengirim
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
